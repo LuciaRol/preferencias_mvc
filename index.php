f434694e81f679da520db066efb14994c4a4ae3a
Lucía <luciarol87@gmail.com>
2024-08-21 12:26:05 +0200

    PRUEBA

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
-    <title>Establecer preferencias</title>
+    <title>Establecer preferencias ESTO ES UNA PRUEBA</title>
 </head>
 
 <body>
